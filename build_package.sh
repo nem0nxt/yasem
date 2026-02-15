250ee7138147d5be73e0a10a76109e13277bbd73
nem0nxt <nem0nxt@users.noreply.github.com>
2026-02-15 02:47:30 +0200

    Harden build scripts (paths, cd checks, libav dir)

--- build_package.sh
+++ build_package.sh
@@ -5,9 +5,9 @@ YASEM_OUT_DIR="yasem-$(uname -m)-$(date +"%F_%H-%M-%S")-$(git rev-parse --short 
 FULL_OUTPUT_DIR="${TMP_DIR}/${YASEM_OUT_DIR}"
 CPU_COUNT=$(nproc)
 
-$QTDIR/gcc_${ARCH}/bin/qmake -recursive YASEM_OUTPUT_DIR=${FULL_OUTPUT_DIR}
-make -j${CPU_COUNT}
-cd ${TMP_DIR}
-tar -zcvf $1/${YASEM_OUT_DIR}.tar.gz ${YASEM_OUT_DIR}
-cd -
-rm "${FULL_OUTPUT_DIR}" -r
+"$QTDIR/gcc_${ARCH}/bin/qmake" -recursive "YASEM_OUTPUT_DIR=${FULL_OUTPUT_DIR}"
+make -j"${CPU_COUNT}"
+cd "${TMP_DIR}" || exit 1
+tar -zcvf "$1/${YASEM_OUT_DIR}.tar.gz" "${YASEM_OUT_DIR}"
+cd - || exit 1
+rm -r -- "${FULL_OUTPUT_DIR}"
--- update_dependencies.sh
+++ update_dependencies.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
-SOURCES_DIR=`pwd`
-THIRD_PARTY_DIR=$SOURCES_DIR/third_party
+SOURCES_DIR=$(pwd)
+THIRD_PARTY_DIR="$SOURCES_DIR/third_party"
 
-LIBAV_DIR=$THIRD_PARTY_DIR/libav
+LIBAV_DIR="$THIRD_PARTY_DIR/libav"
 
-if [ ! -d $THIRD_PARTY_DIR ]; then
-    mkdir $THIRD_PARTY_DIR
+if [ ! -d "$THIRD_PARTY_DIR" ]; then
+    mkdir -p "$THIRD_PARTY_DIR"
 fi
 
-if [ ! -d $LIBAV_DIR ]; then
-    git clone git://git.libav.org/libav.git $LIBAV_DIR
+if [ ! -d "$LIBAV_DIR" ]; then
+    git clone git://git.libav.org/libav.git "$LIBAV_DIR"
 else
-    cd $LIBAV_DIR && git pull origin master
+    (cd "$LIBAV_DIR" && git pull origin master)
 fi
 
+cd "$LIBAV_DIR" || exit 1
 ./configure
 make
 
-cd $SOURCES_DIR
+cd "$SOURCES_DIR" || exit 1
 
